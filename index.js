fa026f58e62ada535c306c259a7052808531f4de
Kartik Verma <kartikvr20@gmail.com>
2024-03-05 01:04:56 +0530

    Added dotenv, sequelize

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
+if (process.env.NODE_ENV !== 'production') {
+    require("dotenv").config();
+}
 const express = require("express");
 const app = express();
+
+const sequelize = require("./database/config")
+
+async function connectToDatabase() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+
+connectToDatabase();
+
+
 
 const path = require("path");
 
@@ -9,6 +27,7 @@ const authRoutes = require("./routes/auth");
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", pagesRoutes);
 app.use("/", authRoutes);

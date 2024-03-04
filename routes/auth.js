93f07f40eff67bd10e2128f75c6fa567b7852467
Kartik Verma <kartikvr20@gmail.com>
2024-03-05 01:06:38 +0530

    Initialised create and check username routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const Router = express.Router();
 
+const User = require("../models/User")
+
 Router.get("/create", (req, res) => {
     res.render("createAccount");
 });
 
-Router.post("/create", (req, res) => {});
+Router.post("/create", async (req, res) => {
+    const { username, email, password, mobile, gender } = req.body;
+    let newUser = await User.create({
+        username, passwordhash: password, gender, mobile, email
+    })
+    res.redirect("/")
+});
+
+Router.get('/check/:username', async (req, res, next) => {
+    const name = req.params.username;
+    const user = await User.findOne({ where: { username: name } });
+
+    if (user) {
+        res.json({ available: false });
+    }
+
+    else res.json({ available: true });
+})
 
 Router.get("/signin", (req, res) => {
     res.render("signin");
 });
 
-Router.post("/signin", (req, res) => {});
+Router.post("/signin", (req, res) => { });
 
 module.exports = Router;

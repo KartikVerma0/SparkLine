ea32c87df074d1aa7440bd76f8c9ae01f17861db
Kartik Verma <kartikvr20@gmail.com>
2024-03-05 01:05:38 +0530

    Initial commit

---
+++ models/User.js
@@ -0,0 +1,38 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../database/config");
+
+const User = sequelize.define('users', {
+    userid: {
+        type: DataTypes.NUMBER,
+        unique: true,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+    },
+    passwordhash: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    gender: {
+        type: DataTypes.STRING,
+    },
+    mobile: {
+        type: DataTypes.NUMBER
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    pic: {
+        type: DataTypes.STRING
+    },
+    profiletext: {
+        type: DataTypes.STRING
+    }
+}, { createdAt: false, updatedAt: false });
+
+module.exports = User;

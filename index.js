7fed9c4a434abbfc26ef2d50ff0426a4a0d834ad
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-03-06 23:10:45 +0530

    ADDED express-session, flash

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const express = require("express");
 const app = express();
+const session = require("express-session");
+const flash = require("connect-flash");
 
 const sequelize = require("./database/config")
 
@@ -27,7 +29,14 @@ const authRoutes = require("./routes/auth");
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true
+}))
+app.use(flash());
 
 app.use("/", pagesRoutes);
 app.use("/", authRoutes);

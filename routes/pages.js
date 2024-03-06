10b63c5ee3267fa94ae9ec05af2a999aa349d1d1
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-03-06 23:12:38 +0530

    MINOR UPDATE

--- routes/pages.js
+++ routes/pages.js
@@ -2,7 +2,7 @@ const express = require("express");
 const Router = express.Router();
 
 Router.get("/", (req, res) => {
-    res.render("home");
+    res.render("home", { message: req.flash("success"), loggedInUser: req.session.username });
 });
 
 module.exports = Router;

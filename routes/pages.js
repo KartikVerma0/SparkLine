98d462f5b79a7cc2461733e3543edef461f78585
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-03-11 19:47:24 +0530

    "Post" routes shifted to post.js route file

--- routes/pages.js
+++ routes/pages.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const Router = express.Router();
 
-const multer = require("multer");
-
-const upload = multer({ dest: 'public/uploads/posts' })
-
 const User = require("../models/User");
-const Post = require("../models/Post");
+
+
+const postRoute = require('./post');
 
 const { isLoggedIn } = require("../middleware");
+
+Router.use('/posts', postRoute);
 
 Router.get("/", (req, res) => {
     res.render("home", { message: req.flash("success"), status: true, loggedInUser: req.session.username });
@@ -25,33 +25,5 @@ Router.get("/dashboard", isLoggedIn, async (req, res) => {
 
 });
 
-Router.get("/posts", isLoggedIn, async (req, res) => {
-    const username = req.session.username;
-    const loggedInUser = await User.findOne({ where: { username: username } });
-    const posts = await Post.findAll({
-        include: [{ model: User, attributes: ['username', 'pic'] }]
-    });
-    const isSuccessMessage = req.flash("success").length ? true : false;
-
-    res.render('posts', { loggedInUser, message: isSuccessMessage ? req.flash('success') : req.flash('error'), status: isSuccessMessage ? true : false, posts });
-})
-
-Router.post("/posts", upload.single('media'), async (req, res) => {
-    const { posttext } = req.body;
-    const media = req.file;
-    const userid = req.session.userid;
-    console.log(posttext, media.path, userid);
-    try {
-        await Post.create({
-            posttext: posttext,
-            userid: userid,
-            media: media.path.substring(7),
-        })
-        req.flash("success", "Created new post")
-    } catch (e) {
-        req.flash("error", e.message)
-    }
-    res.redirect('/posts');
-})
 
 module.exports = Router;

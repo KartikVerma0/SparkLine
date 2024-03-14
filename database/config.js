9042119b0d42a33d34f5a2545c6ed468b964c785
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-03-14 23:05:47 +0530

    Added info from env file

--- database/config.js
+++ database/config.js
@@ -1,7 +1,10 @@
 const { Sequelize } = require("sequelize");
+const mysql2 = require("mysql2");
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USERNAME, process.env.DATABASE_PWD, {
-    host: 'localhost',
-    dialect: 'mysql'
+    host: process.env.DATABASE_HOSTNAME,
+    port: process.env.DATABASE_PORT,
+    dialect: 'mysql',
+    dialectModule: mysql2
 })
 
 module.exports = sequelize;

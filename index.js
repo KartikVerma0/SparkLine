e00d61f16e6153bb6f602e35bc10138878e57d05
Kartik Verma <katik2129.be21@chitkara.edu.in>
2024-03-15 10:10:13 +0530

    Added MySQL session Store

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const app = express();
 const session = require("express-session");
 const flash = require("connect-flash");
+const MySQLStore = require("express-mysql-session")(session);
 
 const sequelize = require("./database/config")
 
@@ -30,11 +31,30 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const sessionStore = new MySQLStore({
+    host: process.env.DATABASE_HOSTNAME,
+    port: process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PWD,
+    database: process.env.DATABASE_NAME
+})
+
 app.use(session({
     secret: process.env.SESSION_SECRET,
     resave: true,
-    saveUninitialized: true
+    saveUninitialized: true,
+    store: sessionStore
 }))
+
+sessionStore.onReady().then(() => {
+    // MySQL session store ready for use.
+    console.log('MySQLStore ready');
+}).catch(error => {
+    // Something went wrong.
+    console.error(error);
+});
+
 app.use(flash());
 
 app.use("/", pagesRoutes);
